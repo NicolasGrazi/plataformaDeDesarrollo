ab2120f5da1b6bce0efdc39001b5df87e66519e1
GrazianoNicolas <87537145+GrazianoNicolas@users.noreply.github.com>
2022-10-07 18:30:47 -0300

    Add files via upload

---
+++ Ejercicio 1 (2)/Ejercicio 1/Ejercicio 1/Pago.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Pago
+    {
+        private int id { get; set; }
+        private Usuario user { get; set; }
+        private String nombre { get; set; }
+        private float monto { get; set; }
+        private bool pagado { get; set; }
+        private String metodo { get; set; }
+
+
+
+
+
+    }
+}
