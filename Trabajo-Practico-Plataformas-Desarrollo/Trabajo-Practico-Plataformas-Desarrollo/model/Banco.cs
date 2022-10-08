d23da1841a6b21403cd45501204e6fddc45bd205
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-10-08 17:26:09 -0300

    Cambio Bnaco

--- Trabajo-Practico-Plataformas-Desarrollo/Trabajo-Practico-Plataformas-Desarrollo/model/Banco.cs
+++ Trabajo-Practico-Plataformas-Desarrollo/Trabajo-Practico-Plataformas-Desarrollo/model/Banco.cs
@@ -26,5 +26,115 @@ namespace Trabajo_Practico_Plataformas_Desarrollo.model
             usuario = new List<Usuario>();
 
         }
+        /*
+         * SI SE DESCOMENTA ROMPE LA APLICACION
+        public void IniciarSesion(string usuario, string contrasenia)
+        {
+            bool encontrado = false
+
+
+            foreach (Usuario usuarioInd in this.usuario)
+            {
+                if (usuarioInd.nombre == usuario)
+
+                {
+                    if (usuarioInd.bloqueado == false)
+                    {
+
+                        while (usuarioInd.intentosFallidos < 4 && encontrado == false)
+                        {
+
+
+                            if (usuarioInd.password == contrasenia)
+                            {
+                                usuarioActual = usuarioInd;
+                                usuarioInd.intentosFallidos = 0;
+                                encontrado = true;
+                            }
+                            else
+                            {
+                                usuarioInd.intentosFallidos++;
+                                if (usuarioInd.intentosFallidos == 4)
+                                {
+                                    usuarioInd.bloqueado = true
+                                }
+
+                            }
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+
+        
+           public List<CajaDeAhorro> MostrarCajasDeAhorro()
+          {
+               return this.usuarioActual.cajas;
+          }
+         
+
+        public void CerrarSesion()
+        {
+            usuarioActual = null;
+        }
+
+        public void CrearCajaAhorro()
+        {
+            if (usuarioActual != null)
+            {
+                // Creás la caja de ahorro.
+            }
+        }
+
+        public void Depositar()
+        {
+            if (usuarioActual != null)
+            {
+                // Depositás.
+            }
+        }
+        public void Retirar()
+        {
+            if (usuarioActual != null)
+            {
+                // Retirás.
+            }
+        }
+
+        public List<Pago> MostrarPagos()
+        {
+            return usuarioActual.pagos;
+
+
+        }
+
+        public List<PlazoFijo> MostrarPlazoFijos()
+        {
+            return usuarioActual.pf;
+        }
+
+        public List<Movimiento> MostrarMovimientos(CajaDeAhorro numero)
+        {
+            if (usuarioActual != null)
+            {
+                foreach (Usuario usuarioInd in this.usuarioActual.cajas)
+                {
+                    if (numero.id == ((CajaDeAhorro)usuarioInd).id)
+                    {
+                        return numero.movimientos;
+
+                    }
+
+                }
+            }
+        }
+
+        */
+
     }
+
 }
+
